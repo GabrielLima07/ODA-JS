298854946c129cefaa156ccd0408ff3d192af9ee
GabrielLima07 <102686294+GabrielLima07@users.noreply.github.com>
2022-07-13 19:46:02 -0300

    Update P02-VSCode.js

--- P02-VSCode.js
+++ P02-VSCode.js
@@ -1,8 +1,8 @@
 /*
 O VS Code foi a melhor escolha por várias razões, dentre elas estão: Sua imensa gama de atalhos e funcionalidades;
-A possibilidade de customiza-lo; Não servir apenas para JavaScript, mas também para outras tecnologias; Uma loja cheia de extensões úteis.
-Por enquanto são as poucas as extensões que estou usando, Auto Close Tag e Auto Rename Tag para agilizar tarefas repetitivas, como a de 
-fechar e abrir as tags no HTML e de renomear a tag. Na parte visual o tema usado está sendo o Dracula. Para facilitar a identificação das
+A possibilidade de customizá-lo; Não serve apenas para JavaScript, mas também para outras tecnologias; Uma loja cheia de extensões úteis.
+Por enquanto são as poucas as extensões que estou usando, Auto Close Tag e Auto Rename Tag para agilizar tarefas repetitivas, como a de
+fechar e abrir as tags no HTML e renomear a tag. Na parte visual o tema usado está sendo o Dracula. Para facilitar a identificação das
 cores através do seu código hexadecimal optei pelo Color Highlight e na intenção de destacar o que precisar ser corrigido no código e/ou
 o que falta ser feito o TODO Highlight será de suma importância.
 */ 
