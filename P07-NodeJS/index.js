33d3656a2b4b702c85bbdb3950d8f49d120e0286
GabrielLima07 <102686294+GabrielLima07@users.noreply.github.com>
2022-07-26 10:01:05 -0300

    Add files via upload

---
+++ P07-NodeJS/index.js
@@ -0,0 +1,25 @@
+// Node.js
+function cabecalho() {
+    return "=======================================\n        SISTEMA DE NOTAS ESCOLAR        \n======================================="
+}
+
+var input = require('readline-sync')
+
+console.log(cabecalho())
+
+const n1 = Number(input.question("Insira a primeira nota: "))
+const n2 = Number(input.question("Insira a segunda nota: "))
+const n3 = Number(input.question("Insira a terceira nota: "))
+const media = ((n1 + n2 + n3) / 3)
+
+console.clear()
+console.log(cabecalho())
+
+console.log(
+`Nota 1: ${n1}
+Nota 2: ${n2}
+Nota 3: ${n3}
+Média: ${media}`)
+
+let situacao = (media >= 7 ? "Aluno APROVADO" : "Aluno REPROVADO")
+console.log(situacao)
